da490b43b5a9a68a0edc15f222a214193046cba8
Raidiniom <raidiniom22PH@gmail.com>
2024-06-20 08:05:10 +0800

    ourdb is testing ground for access to the database

---
+++ src/pages/ourdb.js
@@ -0,0 +1,31 @@
+import '../styles/sitestyle.css'
+
+const Db = () => {
+    return (
+        <div class="wholesite">
+            <div class="input">
+                <div class="log">
+                    <h2>Login</h2>
+                    {/*  */}
+                </div>
+
+                <div class="reg">
+                    <h2>Register</h2>  
+                    {/*  */}
+                </div>
+
+                <div class="rec">
+                    <h2>Recover</h2>
+                    {/*  */}
+                </div>
+
+                <div class="crr">
+                    <h2>Create Race</h2>
+                    {/*  */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Db
