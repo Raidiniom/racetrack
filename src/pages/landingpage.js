ccbed0d405679b601f5d1daa6d5760a3f40bc100
CallenCaracy <diangcocalen@gmail.com>
2024-06-20 09:50:11 +0800

    nav bar pani

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -4,8 +4,38 @@ import '../styles/sitestyle.css'
 const Landingpage = () => {
     return (
         <div class="wholesite">
+
+            {/* Navbar */}
+            <div>
+                <nav class="navbar">
+
+                    {/* Logo */}
+                    <div className="navbar-logo">
+                        <p>RaceTrack</p>
+                    </div>
+                
+                    {/* navbar login and sign up */}
+                    <div className="navbar-links">
+                        <NavLink to="/create" className="nav-link">
+                            <button className="nav-button">Login</button>
+                        </NavLink>
+                        <NavLink to="/create" className="nav-link">
+                            <button className="nav-button">Sign Up</button>
+                        </NavLink>
+                    </div>
+                    
+                </nav>
+            </div>
+
             <h2>Landingpage</h2>
             <p>THIS THE Landingpage</p>
+
+            {/* footer */}
+                <div className='footer'>
+                    <NavLink to="/create">About Us</NavLink>
+                    <NavLink to="/create">Terms of Service</NavLink>
+                    <NavLink to="/create">End-User License Agreement</NavLink>
+                </div>
         </div>
     )
 }
