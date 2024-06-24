fdbc730d8e23007acad81b187bf22fffe7ffe888
CallenCaracy <diangcocalen@gmail.com>
2024-06-24 10:04:51 +0800

    form

--- src/pages/createRace.js
+++ src/pages/createRace.js
@@ -9,19 +9,24 @@ const Creater = () => {
                     <div className="create-form-container">
                         <h2 className="l-h2">Create Race</h2>
 
-                        {/* login form */}
+                        {/* create form */}
                         <form className="create-form">
 
-                            {/* input email */}
-                            <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" name="email" required />
+                            {/* input title */}
+                            <label htmlFor="title">Title of the Race:</label>
+                            <input type="title" id="title" name="title" required />
                             
-                            {/* input password */}
-                            <label htmlFor="password">Password:</label>
-                            <input type="password" id="password" name="password" required />
+                            {/* input start date */}
+                            <label htmlFor="startDate">Start Date:</label>
+                            <input type="date" id="startDate" name="startDate" required />
 
-                            {/* forgot password? */}
-                            <NavLink to="/recover" className="forgot-password-link">Forgot Password?</NavLink>
+                            {/* input registration date */}
+                            <label htmlFor="registrationDate">Registration Date:</label>
+                            <input type="date" id="registrationDate" name="registrationDate" required />
+
+                            {/* input capacity */}
+                            <label htmlFor="capacity">Capacity:</label>
+                            <input type="number" id="capacity" name="capacity" required />
                             
                             {/* submit */}
                             <button type="submit" className="create-button">Create</button>
