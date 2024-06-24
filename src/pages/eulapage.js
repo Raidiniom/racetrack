ad03eb97fad3ec8ca418593cb7f530e18f454602
Raidiniom <raidiniom22PH@gmail.com>
2024-06-24 08:45:15 +0800

    naay nay EULA and TOS pages

---
+++ src/pages/eulapage.js
@@ -0,0 +1,13 @@
+import { NavLink } from 'react-router-dom'
+import '../styles/sitestyle.css'
+
+const Eula = () => {
+
+    return (
+        <div className='wholesite'>
+            <h1>THIS PAGE FOR END USER LICENSE AGREEMENT</h1>
+        </div>
+    )
+}
+
+export default Eula
