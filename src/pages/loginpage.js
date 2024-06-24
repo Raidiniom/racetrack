56505c1d5722d5148226bf53b7340d493cdfa155
CallenCaracy <diangcocalen@gmail.com>
2024-06-24 09:33:22 +0800

    ill continue editing

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -5,28 +5,9 @@ const Login = () => {
     return (
         <div className="wholesite">
 
-             {/* Navbar */}
-            <div>
-                <nav className="navbar">
-
-                    {/* Logo */}
-                    <div className="navbar-logo">
-                        <img src="\img\RaceTrack Logos\2.png" alt="logo" className="RaceTrack-logo" />
-                    </div>
-                
-                    {/* navbar login and sign up */}
-                    <div className="navbar-links">
-                        <NavLink to="/login" className="nav-link">
-                            <button className="nav-button">Login</button>
-                        </NavLink>
-                        <NavLink to="/register" className="nav-link">
-                            <button className="nav-button">Sign Up</button>
-                        </NavLink>
-                    </div>
-                    
-                </nav>
+            <div className="logo">
+                <img src="\img\RaceTrack Logos\2.png" alt="logo" className="RaceTrack-logo" />
             </div>
-
             {/* login form */}
             <div className="main-container">
                 <div className="login-form-position">
@@ -53,31 +34,6 @@ const Login = () => {
                     </div>
                 </div>
             </div>
-
-            {/* footer */}
-            <footer className='footer'>
-                    <div className='footer-container'>
-                        <div className='group_ab_tos_eula'>
-                            <NavLink to="/about" className="footer-link">
-                                <li>About Us</li>
-                            </NavLink>
-                            <NavLink to="/terms" className="footer-link">
-                                <li>Terms of Service</li>
-                            </NavLink>
-                            <NavLink to="/eula" className="footer-link">
-                                <li>End-User License Agreement</li>
-                            </NavLink>
-                        </div>
-                        <div className="contact-us">
-                            <p className='contact-us'>Contact Us</p>
-                            <div className="footer-img-container">
-                                <img src="/img/fb.png" alt="Facebook" className="footer-image-fb" />
-                                <img src="/img/IG.png" alt="Instagram" className="footer-image-ig" />
-                                <img src="/img/message.png" alt="Message" className="footer-image-m" />
-                            </div>
-                        </div>
-                    </div>
-                </footer>
         </div>
     )
 }
