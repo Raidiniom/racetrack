e031aa0d9f6bb7dedd70355ea5af5189890e089a
CallenCaracy <diangcocalen@gmail.com>
2024-06-24 09:51:21 +0800

    create init

--- src/pages/createRace.js
+++ src/pages/createRace.js
@@ -4,9 +4,33 @@ import '../styles/sitestyle.css'
 const Creater = () => {
     return (
         <div className="wholesite">
-            <h2>Create Race</h2>
-            <p>THIS PAGE IS FOR CREATING A RACE</p>
+            <div className="main-container">
+                <div className="create-form-position">
+                    <div className="create-form-container">
+                        <h2 className="l-h2">Create Race</h2>
+
+                        {/* login form */}
+                        <form className="create-form">
+
+                            {/* input email */}
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" id="email" name="email" required />
+                            
+                            {/* input password */}
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" id="password" name="password" required />
+
+                            {/* forgot password? */}
+                            <NavLink to="/recover" className="forgot-password-link">Forgot Password?</NavLink>
+                            
+                            {/* submit */}
+                            <button type="submit" className="create-button">Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
