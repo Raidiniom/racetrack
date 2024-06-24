6c3a8bc490eda1355b266fdaf5b023774af9d175
CallenCaracy <diangcocalen@gmail.com>
2024-06-24 21:16:02 +0800

    login edit

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -4,33 +4,39 @@ import '../styles/loginpage.css'
 const Login = () => {
     return (
         <div className="wholesite">
+            <div className="whole-2">
 
-            <div className="logo">
-                <img src="\img\RaceTrack Logos\2.png" alt="logo" className="RaceTrack-logo" />
-            </div>
-            {/* login form */}
-            <div className="main-container">
-                <div className="login-form-position">
-                    <div className="login-form-container">
-                        <h2 className="l-h2">Login</h2>
+                    {/* logo */}
+                <div className="login-logo">
+                    <img src="\img\RaceTrack Logos\2.png" alt="logo" className="RaceTrack-logo" />
+                </div>
 
-                        {/* login form */}
-                        <form className="login-form">
+                {/* login form */}
+                <div className="login-main-container">
 
-                            {/* input email */}
-                            <label htmlFor="email">Email:</label>
-                            <input type="email" id="email" name="email" required />
-                            
-                            {/* input password */}
-                            <label htmlFor="password">Password:</label>
-                            <input type="password" id="password" name="password" required />
+                    <div className="login-form-position">
+                        <div className="login-form-container">
 
-                            {/* forgot password? */}
-                            <NavLink to="/recover" className="forgot-password-link">Forgot Password?</NavLink>
-                            
-                            {/* submit */}
-                            <button type="submit" className="login-button">Login</button>
-                        </form>
+                            <h2 className="l-h2">Login</h2>
+
+                            {/* login form */}
+                            <form className="login-form">
+
+                                {/* input email */}
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" id="email" name="email" required />
+                                
+                                {/* input password */}
+                                <label htmlFor="password">Password:</label>
+                                <input type="password" id="password" name="password" required />
+
+                                {/* forgot password? */}
+                                <NavLink to="/recover" className="forgot-password-link">Forgot Password?</NavLink>
+                                
+                                {/* submit */}
+                                <button type="submit" className="login-button">Login</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
