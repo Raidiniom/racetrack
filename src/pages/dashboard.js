ff986e878117e153eb616597802882a92e409c3e
CallenCaracy <diangcocalen@gmail.com>
2024-06-29 18:05:43 +0800

    dashboard initialized

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,16 +1,36 @@
 import { NavLink } from 'react-router-dom'
-import '../styles/sitestyle.css'
+import '../styles/dashboardpage.css'
 
 const Dashboard = () => {
     return (
         <div className="wholesite">
-            <div className="header">
-                <h1>RaceTrack</h1>
-
-                <nav>
-                    <NavLink to="/create">Create Race</NavLink>
-                </nav>
+            <div class="dashboard">
+                <div class="dashboard-header">
+                    <h1>RaceTrack</h1>
+                </div>
+                <div class="dashboard-sidebar">
+                    <ul>
+                        <li>Profile</li>
+                        <li><NavLink to="/create">Create Race</NavLink></li>
+                        <li><NavLink to="/update">Update Race</NavLink></li>
+                        <li>Settings</li>
+                        <li><NavLink to="/login">Logout</NavLink></li>
+                    </ul>
+                </div>
+                <div class="dashboard-main-content">
+                    <h2>Main Content</h2>
+                    <div class="card">Card 1</div>
+                    <div class="card">Card 2</div>
+                    <div class="card">Card 3</div>
+                </div>
+                <div class="dashboard-footer">
+                    <p>???</p>
+                </div>
             </div>
+                <nav>
+                    
+                </nav>
+            
         </div>
     )
 }
