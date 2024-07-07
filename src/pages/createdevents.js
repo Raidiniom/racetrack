610eb3bb56f13f48e0bc46d25ba35b017d556e90
CallenCaracy <diangcocalen@gmail.com>
2024-07-07 20:03:55 +0800

    user profile and created events added

---
+++ src/pages/createdevents.js
@@ -0,0 +1,12 @@
+import { NavLink } from 'react-router-dom'
+import '../styles/sitestyle.css'
+
+const MadeEvents = () => {
+    return (
+        <div className="wholesite">
+            <p>mga events nimo sus!</p>
+        </div>
+    )
+}
+
+export default MadeEvents
