11288c19ec6330f31a5fd0eb249f01f3a3e3cec1
CallenCaracy <diangcocalen@gmail.com>
2024-07-22 12:00:22 +0800

    button

--- src/pages/notifications.js
+++ src/pages/notifications.js
@@ -46,6 +46,22 @@ const Notifications = () => {
                 <div className="gen-headerbar-logo">
                     <NavLink to='/dashboard'><img src="/img/RaceTrack Logos/RT-logo.png" alt="logo" className="RaceTrack-logo" /></NavLink>
                 </div>
+                <div className="header-right">
+                        <div className="pfp">
+                            <NavLink to="/profile">
+                                <button className="notification-button">
+                                    <img src="img/pfp.png" alt="icon" className="pfp-icon" /> Profile
+                                </button>
+                            </NavLink>
+                        </div>
+                        <div className="notifications-container">
+                            <NavLink to="/notifications">
+                                <button className="notification-button">
+                                    <img src="img/noti-icon.png" alt="icon" className="noti-icon" /> Notifications
+                                </button>
+                            </NavLink>
+                        </div>
+                    </div>
             </div>
             {/* Sidebar */}
             <div className="gen-sidebar">
