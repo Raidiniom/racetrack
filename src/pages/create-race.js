0e667487bfe2c5c88a3e4b52e9c3ebc0e3fb774d
samantiporta <155541493+samantiporta@users.noreply.github.com>
2025-10-22 15:09:12 +0800

    Update create-race.js
    add category service into create race

--- src/pages/create-race.js
+++ src/pages/create-race.js
@@ -3,6 +3,7 @@ import '../styles/create-page.css'
 import '../styles/header_and_sidebar.css'
 import { useState } from 'react'
 import supabase from "../config/supabaseclient"
+import { getCategories, findCategoryByDistance } from '../services/categoryServices'
 
 const Db = () => {
 
